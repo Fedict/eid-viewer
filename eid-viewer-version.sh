e824b156dcd558651a6f1f1094c078107caf7808
Frank Mariën <frank.marien@fedict.be>
2012-03-08 15:28:14 +0000

    added logic to automatically set version in configure.ac and svn version text files added automatic version and other metadata including revision in exe and osx bundle

---
+++ eid-viewer-version.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+VERSIONFILE="eid-viewer-version"
+
+if [ -f "$VERSIONFILE" ]; then
+    cat $VERSIONFILE
+else
+    VERSION=`svn info | grep ^URL | cut -d " " -f 2 | awk -F/ '{print $NF}'`
+    REVISION=`svnversion`
+    if [ "$VERSION" == "trunk" ]; then VERSION="0.0.0"; fi
+    echo -n $VERSION-$REVISION | tee $VERSIONFILE
+    echo -n $REVISION > svn_revision
+fi
+
