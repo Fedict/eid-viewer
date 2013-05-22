9382a50e26c433ae833b776ed5ecd195a4ead1ae
Frank Mariën <frank.marien@fedict.be>
2013-05-22 14:23:02 +0000

    updated copyright dates

--- eid-viewer-lib/src/main/java/be/fedict/eidviewer/lib/file/gui/EidFilePreviewAccessory.java
+++ eid-viewer-lib/src/main/java/be/fedict/eidviewer/lib/file/gui/EidFilePreviewAccessory.java
@@ -1,6 +1,6 @@
 /*
  * eID Middleware Project.
- * Copyright (C) 2010-2012 FedICT.
+ * Copyright (C) 2010-2013 FedICT.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version
