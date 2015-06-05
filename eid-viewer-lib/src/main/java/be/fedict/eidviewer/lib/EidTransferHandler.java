d737c7e0d1229a58bac6de7bb4fde2864fe290c3
Wouter Verhelst <w@uter.be>
2015-06-05 12:30:23 +0200

    Add some logging and error handling

--- eid-viewer-lib/src/main/java/be/fedict/eidviewer/lib/EidTransferHandler.java
+++ eid-viewer-lib/src/main/java/be/fedict/eidviewer/lib/EidTransferHandler.java
@@ -12,18 +12,37 @@ public class EidTransferHandler extends TransferHandler {
 	 * 
 	 */
 	private static final long serialVersionUID = -402732694948104329L;
+	private static final Logger logger = Logger.getLogger(EidTransferHandler.class.getName());
 	private PCSCEidController ctrl;
 	public EidTransferHandler(PCSCEidController eid) {
 		ctrl = eid;
 	}
 	@Override
 	public Transferable createTransferable(JComponent comp) {
+		logger.fine("Attempting drag-and-drop operation");
+		switch(ctrl.getState()) {
+		case EID_PRESENT:
+		case FILE_LOADED:
+			break;
+		default:
+			logger.fine("No data, aborting drag-and-drop operation");
+			return null;
+		}
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		ctrl.saveToXMLFile(os);
+		try {
+			ctrl.saveToXMLFile(os);
+		} catch (Exception e) {
+			return null;
+		}
+		logger.fine("Creating data handler for drag-and-drop operation...");
 		return new StringSelection(os.toString());
 	}
 	@Override
 	public int getSourceActions(JComponent c) {
-		return COPY;
+		if(eid.equals(null)) {
+			return javax.swing.TransferHandler.NONE;
+		}
+		logger.fine("enabling drag-and-drop");
+		return javax.swing.TransferHandler.COPY;
 	}
 }
